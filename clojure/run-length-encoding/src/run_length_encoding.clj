54721cd1a740e55b8e3fd84cd2e48ad289f1fda1
Jeff <zhangzhuzhefu@gmail.com>
2017-11-15 23:52:15 +0800

    run-length-encode improvment

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,19 +1,20 @@
 (ns run-length-encoding)
 
-(defn- combine [consecutive]
+(defn- combine [[consecutive ch]]
 	(let [cnt (count consecutive)]
-		(if (= cnt 1) consecutive
-			(str cnt (first consecutive)))))
+		(if (= cnt 1) ch
+			(str cnt ch))))
 
-(defn- expand [code]
+(defn- expand [[code ch]]
 	(let [cnt (count code)]
-		(if (= cnt 1) code
-			(apply str (take (read-string (subs code 0 (dec cnt))) (repeat (last code)))))))
+		(if (= cnt 1) ch 
+			(let [repeats (read-string (subs code 0 (dec cnt)))]
+				(apply str (repeat repeats ch))))))
 
 (defn run-length-encode [s]
 	(if (= s "") ""
-		(apply str (map combine (first (apply map vector (re-seq #"(.)\1{0,}" s)))))))
+		(apply str (map combine (re-seq #"(.)\1{0,}" s)))))
 
 (defn run-length-decode [s]
 	(if (= s "") ""
- 	 	(apply str (map expand (first (apply map vector (re-seq #"\d*(.)" s)))))))
+ 	 	(apply str (map expand (re-seq #"\d*(.)" s)))))
