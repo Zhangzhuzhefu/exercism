397ab7619c380d5e478bca5c367ea11f7745cf55
Jeff <zhangzhuzhefu@gmail.com>
2018-03-20 23:44:11 +0800

    clear trailing space in beer_song

--- clojure/beer-song/src/beer_song.clj
+++ clojure/beer-song/src/beer_song.clj
@@ -10,18 +10,18 @@
 )
 
 (defn verse [n]
-	(if (= n 0) 
-		(str "No more bottles of beer on the wall, no more bottles of beer.\n" 
+	(if (= n 0)
+		(str "No more bottles of beer on the wall, no more bottles of beer.\n"
 			"Go to the store and buy some more, 99 bottles of beer on the wall.\n")
 		(str (format "%d %s of beer on the wall, %d %s of beer.\n" n (bottles n) n (bottles n))
-			(format "Take %s down and pass it around, %s %s of beer on the wall.\n" 
-				(one-it n) 
-				(if (= n 1) (str "no more") (- n 1)) 
+			(format "Take %s down and pass it around, %s %s of beer on the wall.\n"
+				(one-it n)
+				(if (= n 1) (str "no more") (- n 1))
 				(bottles (- n 1)))))
 )
 
 (defn sing ([start] (sing start 0))
-	([start end] 
+	([start end]
 		(loop [song (str) cct start]
 			(if (= cct (- end 1)) (string/triml song)
 				(recur (str song "\n" (verse cct))(dec cct)))))
--- python/hello-world/hello_world_test.py
+++ python/hello-world/hello_world_test.py
@@ -4,7 +4,6 @@ import hello_world
 
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
-
 class HelloWorldTests(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello_world.hello(), 'Hello, World!')
