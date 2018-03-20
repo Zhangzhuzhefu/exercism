7ae8538b2efdd450e2c268bb260f03a1031fba8b
Jeff <zhangzhuzhefu@gmail.com>
2018-03-20 15:29:53 +0800

    check in python helloworld question

--- clojure/anagram/project.clj
+++ clojure/anagram/project.clj
@@ -1,4 +1,5 @@
 (defproject anagram "0.1.0-SNAPSHOT"
+  :aot :all
   :description "anagram exercise."
   :url "https://github.com/exercism/xclojure/tree/master/exercises/anagram"
   :dependencies [[org.clojure/clojure "1.8.0"]])
