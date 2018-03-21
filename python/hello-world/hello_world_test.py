81bec84bed566dc652d0eb66f54f80503ef6da8c
Jeff <zhangzhuzhefu@gmail.com>
2018-03-21 14:02:29 +0800

    Commit Python helloworld

--- python/hello-world/hello_world_test.py
+++ python/hello-world/hello_world_test.py
@@ -8,6 +8,5 @@ class HelloWorldTests(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello_world.hello(), 'Hello, World!')
 
-
 if __name__ == '__main__':
     unittest.main()
