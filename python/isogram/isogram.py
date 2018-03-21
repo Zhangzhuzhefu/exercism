d9b72c5ef51e2a07689f2d93589dd62da396c7cc
Jeff <zhangzhuzhefu@gmail.com>
2018-03-21 22:18:24 +0800

    python isogram using all function

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,11 +1,4 @@
 def is_isogram(string):
     strip = string.replace(" ","").replace("-","").lower()
-    charDict = {}
-    for ch in strip:
-        if charDict.get(ch) is None:
-            charDict[ch] = 1
-        else:
-            charDict[ch] += 1
-            if charDict[ch] > 1:
-                return False
-    return True
+    return all(strip.count(ch) ==1 for ch in strip)
+
