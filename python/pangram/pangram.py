552e50e23e14175e366757ec27d02f64f4cfcfb2
Jeff <zhangzhuzhefu@gmail.com>
2018-03-22 13:57:08 +0800

    Python pangram first solution

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,5 @@
+from string import ascii_lowercase
+
 def is_pangram(sentence):
-    pass
+    sentence = sentence.replace(" ","").lower()
+    return all((ch in sentence) for ch in ascii_lowercase)
