13b87c243726c6eb07d143ee2562d29255e77bc1
Jeff <zhangzhuzhefu@gmail.com>
2018-03-27 16:37:01 +0800

    python gigasecond solution  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:

--- python/gigasecond/gigasecond_test.py
+++ python/gigasecond/gigasecond_test.py
@@ -34,8 +34,8 @@ class GigasecondTest(unittest.TestCase):
 
     def test_yourself(self):
         # customize this to test your birthday and find your gigasecond date:
-        your_birthday = datetime(1970, 1, 1)
-        your_gigasecond = datetime(2001, 9, 9, 1, 46, 40)
+        your_birthday = datetime(1992, 9, 10)
+        your_gigasecond = datetime(2024, 5, 19, 1, 46, 40)
 
         self.assertEqual(add_gigasecond(your_birthday), your_gigasecond)
 
