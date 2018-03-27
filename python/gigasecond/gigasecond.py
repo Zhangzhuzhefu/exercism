b5fbfd5f47a1664da565a3d40407a4813cfeb05d
Jeff <zhangzhuzhefu@gmail.com>
2018-03-27 16:40:39 +0800

    python gigasecond second solution

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -2,4 +2,4 @@ from datetime import datetime
 from datetime import timedelta
 
 def add_gigasecond(birth_date):
-    return birth_date + timedelta(seconds=1000000000)
+    return birth_date + timedelta(seconds=1e9)
