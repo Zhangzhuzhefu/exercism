cd867e3bdafd5c22f3075bd6a94e61e6807bbe52
Jeff <zhangzhuzhefu@gmail.com>
2018-03-28 17:07:37 +0800

    python run-length-encoding regex solution for encode

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -2,16 +2,13 @@ import re
 
 def decode(string):
     codes = re.findall("(\d*)(\D)",string)
-    return "".join([int(c)*w if c.isdigit() else w for c,w in codes])
-
+    return "".join(
+        int(c)*w if c.isdigit() else w for c,w in codes
+    )
 
 def encode(string):
-    words = []
-    count = []
-    for a in string:
-        if not words or words[-1] != a:
-            words.append(a)
-            count.append(1)
-        else:
-            count[-1] += 1
-    return "".join([("" if c == 1 else str(c))+w for w,c in zip(words, count)])
+    return "".join(
+        str(len(ww))+w if len(ww)!=1 else w 
+        for ww, w in re.findall(r'((\D)\2*)', string)
+    )
+
